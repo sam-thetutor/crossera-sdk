67d16792cc0c0ebc250d42cd68bb110e0f244bd2
sam-thetutor <atwebembeiresamuel2020@gmail.com>
2025-10-03 12:51:11 +0300

    Remove node_modules and dist from repository
    - Remove node_modules and dist folders from git tracking
    - These folders should not be committed as they are generated files
    - Update .gitignore to prevent future commits of these folders

--- dist/types/network.d.ts
+++
@@ -1,8 +0,0 @@
-export type Network = 'testnet' | 'mainnet';
-export interface NetworkConfig {
-    baseUrl: string;
-    chainId?: number;
-    name: string;
-}
-export declare const NETWORK_CONFIGS: Record<Network, NetworkConfig>;
-//# sourceMappingURL=network.d.ts.map
--- dist/utils/network.d.ts
+++
@@ -1,5 +0,0 @@
-import { Network } from '../types/network';
-export declare function getNetworkBaseUrl(network: Network): string;
-export declare function getAvailableNetworks(): Network[];
-export declare function getNetworkConfig(network: Network): import("../types/network").NetworkConfig;
-//# sourceMappingURL=network.d.ts.map
